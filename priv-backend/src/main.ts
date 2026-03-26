5762228f691bcfde0f5a97bf0fda1ceb1887aab3
Disha Baghel <bagheldisha708@gmail.com>
2026-03-26 16:56:32 +0530

    prisma config fixes and swagger integration

--- priv-backend/src/main.ts
+++ priv-backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Priv-chat API')
+    .setDescription('API documentation for Priv chat E2EE App')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
