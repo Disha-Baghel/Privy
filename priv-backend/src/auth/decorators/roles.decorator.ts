780c6d955d8fc666244039996e32a0bb61cffd6a
Disha Baghel <bagheldisha708@gmail.com>
2026-04-07 22:33:22 +0530

    backend files update - websocket, webRTC integration for signalling support and p2p chat

--- priv-backend/src/auth/decorators/roles.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { SetMetadata } from "@nestjs/common";
-import { Roles } from "@prisma/client";
-
-export const ROLES_KEY = 'roles';
-
-export const RequiredRoles = (...roles: Roles[]) => 
-    SetMetadata(ROLES_KEY, roles);
